259a9d39e4ab3e2c80f217fb47233c95acaf4a24
IvanUsiel <usiel_jarquin@outlook.com>
2024-11-30 10:10:51 -0600

    second commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolaiOS
+//
+//  Created by Ivan Usiel Ramirez Jarquin on 29/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
